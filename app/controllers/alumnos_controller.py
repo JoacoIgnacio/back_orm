c74e03eea103a2a1f8b9fcb76c4595725f7b4c72
lucascardemil <74120533+lucascardemil@users.noreply.github.com>
2024-07-29 18:26:55 -0400

    Add files via upload

---
+++ app/controllers/alumnos_controller.py
@@ -0,0 +1,170 @@
+from app.BD.conexion import obtener_conexion
+from app.routes.routes_qr import generar_qr_imagen
+from flask import jsonify
+
+def crear_alumno(alumno):
+    qr_info = None
+    try:
+        with obtener_conexion() as conexion:
+            with conexion.cursor() as cursor:
+                # Insertar el nuevo alumno
+                sql_insert = "INSERT INTO alumnos (nombre, apellido, QR, id_curso) VALUES (%s, %s, %s, %s)"
+                cursor.execute(sql_insert, (
+                    alumno['nombre'],
+                    alumno['apellido'],
+                    alumno['QR'],
+                    alumno['id_curso']
+                ))
+                
+                # Obtener el ID del alumno recién creado
+                cursor.execute("SELECT LAST_INSERT_ID()")
+                id_alumno = cursor.fetchone()[0]
+
+                # Crear la información del QR
+                qr_info = {
+                    "nombre": alumno['nombre'],
+                    "apellido": alumno['apellido'],
+                    "id_curso": alumno['id_curso'],
+                    "id_alumno": id_alumno
+                }
+
+                # Generar la imagen del QR
+                qr_path2 = generar_qr_imagen(
+                    alumno['nombre'],
+                    alumno['apellido'],
+                    qr_info
+                )
+
+                # Actualizar la fila del alumno con la nueva información del QR
+                sql_update = "UPDATE alumnos SET QR = %s WHERE id = %s"
+                cursor.execute(sql_update, (qr_path2['ruta_qr'], id_alumno))
+
+            # Confirmar la transacción
+            conexion.commit()
+
+    except Exception as err:
+        print(f'Error al crear alumno: {err}')
+        try:
+            if conexion and not conexion.closed:
+                conexion.rollback()
+        except Exception as rollback_err:
+            print(f'Error al hacer rollback: {rollback_err}')
+    
+    return {"nombre": alumno['nombre'], "apellido": alumno['apellido'], "id_curso": alumno['id_curso'], "id_alumno": id_alumno, "qr": qr_path2['ruta_qr']}
+
+
+
+
+def obtener_alumnos():
+    alumnos = []
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            # Obtener todos los alumnos
+            sql = "SELECT * FROM alumnos"
+            cursor.execute(sql)
+            alumnos = cursor.fetchall()
+    except Exception as err:
+        print('Error al obtener alumnos:', err)
+    finally:
+        if conexion:
+            conexion.close()
+    return alumnos
+
+def obtener_alumnos_por_curso(id_curso):
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            # Consultar todos los alumnos de un curso específico
+            sql = "SELECT * FROM alumnos WHERE id_curso = %s"
+            cursor.execute(sql, (id_curso,))
+            alumnos = cursor.fetchall()
+        print(alumnos)
+        return alumnos
+    except Exception as err:
+        print(f'Error al obtener alumnos por curso {id_curso}: {err}')
+        return []
+    finally:
+        if conexion:
+            conexion.close()
+            
+def obtener_alumno_por_id(alumno_id):
+    alumno = None
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            # Obtener un alumno por ID
+            sql = "SELECT * FROM alumnos WHERE id = %s"
+            cursor.execute(sql, (alumno_id,))
+            alumno = cursor.fetchone()
+    except Exception as err:
+        print(f'Error al obtener alumno con ID {alumno_id}:', err)
+    finally:
+        if conexion:
+            conexion.close()
+    return alumno
+
+def actualizar_alumno(alumno_id, nuevos_datos):
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            # Obtén el ID del curso actual para mantenerlo
+            cursor.execute("SELECT id_curso FROM alumnos WHERE id = %s", (alumno_id,))
+            id_curso_actual = cursor.fetchone()[0]
+
+            # Actualizar un alumno por ID
+            sql = "UPDATE alumnos SET nombre = %s, apellido = %s, QR = %s, id_curso = %s WHERE id = %s"
+
+            # Genera un nuevo QR
+            nuevo_qr_info = f"nombre: {nuevos_datos['nombre']}\napellido: {nuevos_datos['apellido']}\nid_curso: {id_curso_actual}\nid_alumno: {alumno_id}"
+            nuevo_qr = generar_qr_imagen(
+                nuevos_datos['nombre'],
+                nuevos_datos['apellido'],
+                nuevo_qr_info
+            )
+
+            # Extrae la ruta del QR del diccionario
+            nuevo_qr_ruta = nuevo_qr.get('ruta_qr', '')
+
+            cursor.execute(sql, (
+                nuevos_datos['nombre'],
+                nuevos_datos['apellido'],
+                nuevo_qr_ruta,  # Pasa solo la ruta del QR, no el diccionario completo
+                id_curso_actual,  # Usa el ID del curso actual
+                alumno_id
+            ))
+
+        conexion.commit()
+    except Exception as err:
+        print(f'Error al actualizar alumno con ID {alumno_id}:', err)
+    finally:
+        if conexion:
+            conexion.close()
+
+def eliminar_alumno(alumno_id):
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            # Eliminar un alumno por ID
+            sql = "DELETE FROM alumnos WHERE id = %s"
+            cursor.execute(sql, (alumno_id,))
+        conexion.commit()
+    except Exception as err:
+        print(f'Error al eliminar alumno con ID {alumno_id}:', err)
+    finally:
+        if conexion:
+            conexion.close()
+
+def eliminar_alumnos_por_curso(id_curso):
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            # Eliminar alumnos por id_curso
+            sql = "DELETE FROM alumnos WHERE id_curso = %s"
+            cursor.execute(sql, (id_curso,))
+        conexion.commit()
+    except Exception as err:
+        print(f'Error al eliminar alumnos por curso con ID {id_curso}:', err)
+    finally:
+        if conexion:
+            conexion.close()            
---
+++ app/controllers/pruebas_controller.py
@@ -0,0 +1,92 @@
+# app/DB/controllers/pruebas_controller.py
+from app.BD.conexion import obtener_conexion
+
+def crear_prueba(prueba):
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            # Verificar si el id_alumno existe en la tabla alumnos
+            cursor.execute("SELECT id FROM alumnos WHERE id = %s", (prueba['id_alumno'],))
+            id_alumno_existente = cursor.fetchone()
+
+            # Verificar si el id_curso existe en la tabla cursos
+            cursor.execute("SELECT id FROM cursos WHERE id = %s", (prueba['id_curso'],))
+            id_curso_existente = cursor.fetchone()
+
+            if id_alumno_existente and id_curso_existente:
+                # Insertar nueva prueba si ambos existen
+                sql = "INSERT INTO pruebas (nota, activo, id_hoja_de_respuestas, id_curso, id_alumno) VALUES (%s, %s, %s, %s, %s)"
+                cursor.execute(sql, (prueba['nota'], prueba['activo'], prueba['id_hoja_de_respuestas'], prueba['id_curso'], prueba['id_alumno']))
+                conexion.commit()
+            else:
+                print(f"El id_alumno {prueba['id_alumno']} o el id_curso {prueba['id_curso']} no existen en las tablas correspondientes.")
+    except Exception as err:
+        print('Error al crear prueba:', err)
+    finally:
+        if conexion:
+            conexion.close()
+            
+def obtener_pruebas():
+    pruebas = []
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            # Obtener todas las pruebas
+            sql = "SELECT * FROM pruebas"
+            cursor.execute(sql)
+            pruebas = cursor.fetchall()
+    except Exception as err:
+        print('Error al obtener pruebas:', err)
+    finally:
+        if conexion:
+            conexion.close()
+    return pruebas
+
+def obtener_prueba_por_id(prueba_id):
+    prueba = None
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            # Obtener una prueba por ID
+            sql = "SELECT * FROM pruebas WHERE id = %s"
+            cursor.execute(sql, (prueba_id,))
+            prueba = cursor.fetchone()
+    except Exception as err:
+        print(f'Error al obtener prueba con ID {prueba_id}:', err)
+    finally:
+        if conexion:
+            conexion.close()
+    return prueba
+
+def actualizar_prueba(prueba_id, nuevos_datos):
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            # Actualizar una prueba por ID
+            sql = "UPDATE pruebas SET nota = %s, activo = %s, id_hoja_de_respuestas = %s WHERE id = %s"
+            cursor.execute(sql, (
+                nuevos_datos['nota'],
+                nuevos_datos['activo'],
+                nuevos_datos['id_hoja_de_respuestas'],
+                prueba_id
+            ))
+        conexion.commit()
+    except Exception as err:
+        print(f'Error al actualizar prueba con ID {prueba_id}:', err)
+    finally:
+        if conexion:
+            conexion.close()
+
+def eliminar_prueba(prueba_id):
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            # Eliminar una prueba por ID
+            sql = "DELETE FROM pruebas WHERE id = %s"
+            cursor.execute(sql, (prueba_id,))
+        conexion.commit()
+    except Exception as err:
+        print(f'Error al eliminar prueba con ID {prueba_id}:', err)
+    finally:
+        if conexion:
+            conexion.close()
