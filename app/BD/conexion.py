906d8c3c6992523c8a0c4c9ed75ec4b82e32f998
lucascardemil <74120533+lucascardemil@users.noreply.github.com>
2024-07-29 18:25:48 -0400

    Create conexion.py

---
+++ app/BD/conexion.py
@@ -0,0 +1,9 @@
+import pymysql
+
+def obtener_conexion():
+    return pymysql.connect(
+        host='localhost',
+        user='claseac1_lectoromr',
+        password='S%!=vj8sbpy9',  # Agrega tu contraseña real aquí
+        db='claseac1_lectoromr'
+    )
