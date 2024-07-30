1c0d5c1f64a7abe32c210c0ba8183cc7a3eff38d
lucascardemil <74120533+lucascardemil@users.noreply.github.com>
2024-07-30 12:18:05 -0400

    Update conexion.py

--- app/BD/conexion.py
+++ app/BD/conexion.py
@@ -2,8 +2,9 @@ import pymysql
 
 def obtener_conexion():
     return pymysql.connect(
-        host='localhost',
-        user='claseac1_lectoromr',
-        password='S%!=vj8sbpy9',  # Agrega tu contraseña real aquí
-        db='claseac1_lectoromr'
+        host='viaduct.proxy.rlwy.net',
+        user='root',
+        password='mcmMRVpFneUmNZSbZUNwZbtMQwTRaPUV',  # Agrega tu contraseña real aquí
+        db='railway',
+        port=29038  # Asegúrate de que el puerto sea el correcto
     )
