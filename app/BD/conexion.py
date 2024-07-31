145c82be3ce915f970def9bc7979131a5ec834da
lucascardemil <lucas.cardemil@hotmail.com>
2024-07-31 16:10:09 -0400

    actualizacion 31/07/2024

--- app/BD/conexion.py
+++ app/BD/conexion.py
@@ -1,9 +1,18 @@
 import pymysql
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def obtener_conexion():
-    return pymysql.connect(
-        host='localhost',
-        user='claseac1_lectoromr',
-        password='S%!=vj8sbpy9',  # Agrega tu contraseña real aquí
-        db='claseac1_lectoromr'
-    )
+    try:
+        return pymysql.connect(
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            db=os.getenv('DB_NAME'),
+            port=int(os.getenv('DB_PORT'))
+        )
+    except pymysql.MySQLError as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
--- app/routes/routes_scanner.py
+++ app/routes/routes_scanner.py
@@ -5,6 +5,7 @@ from app.controllers.scanner_controller import (
 )
 from app.controllers.respuestas_alumnos_controller import agregar_respuestas_alumnos
 from app.controllers.pruebas_controller import crear_prueba
+import json
 
 scanner_db_bp = Blueprint('scanner', __name__)
 
@@ -18,7 +19,7 @@ def scanner():
             resolver_prueba = solve_test(request_data['ANSWER_KEY'], request_data['alternativas'])
             nota = resolver_prueba.get('nota', 0.0)
             imagen = resolver_prueba.get('image')
-            data_alumno = request_data['data_alumno']
+            data_alumno = procesar_texto(request_data['data_alumno'])
             
             agregar_respuestas_alumnos(request_data['ANSWER_KEY'])
             
@@ -33,8 +34,8 @@ def scanner():
                 'nota': nota,
                 'activo': True  # Puedes ajustar esto según tus necesidades
             }
+                
             
-            print(prueba)
             crear_prueba(prueba)
             
             return jsonify({"status": True, "mensaje": "Se procesó la imagen", "nota": nota, "image": imagen}), 201
@@ -42,3 +43,25 @@ def scanner():
             return jsonify({"status": False, "mensaje": "No se pudo procesar la imagen"}), 500
     except Exception as err:
         return jsonify({"status": False, "error": str(err)}), 500
+
+def procesar_texto(data_alumno):
+    data_alumno_dict = {}
+    try:
+        for line in data_alumno.split('\n'):
+            key, value = line.split(': ')
+            data_alumno_dict[key.strip()] = value.strip()
+    except ValueError:
+        return jsonify({"error": "Formato de data_alumno no es válido"}), 400
+    
+    # Acceder a los valores dentro del diccionario
+    nombre = data_alumno_dict.get('nombre', '')
+    apellido = data_alumno_dict.get('apellido', '')
+    id_curso = data_alumno_dict.get('id_curso', '')
+    id_alumno = data_alumno_dict.get('id_alumno', '')
+
+    return {
+        'nombre': nombre,
+        'apellido': apellido,
+        'id_curso': id_curso,
+        'id_alumno': id_alumno
+    }
