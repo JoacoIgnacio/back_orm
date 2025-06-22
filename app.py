79f84a73a618e586dfec38506d795dd67a0e61c5
Joaquín Ignacio <110521983+JoaquinIgnaci0@users.noreply.github.com>
2025-06-22 02:00:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ db = SQLAlchemy(application)
 def index():
     return "✅ Flask conectado correctamente a MySQL ✅ "
 
-@application.route("z1")
+@application.route("/db-test")
 def db_test():
     try:
         db.session.execute(text("SELECT 1"))
