020bde23fc0419cc68d60c19dbb01b4648a459a5
Joaquín Ignacio <116734135+JoacoIgnacio@users.noreply.github.com>
2025-07-09 23:52:57 -0400

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,11 +2,11 @@ from flask import Blueprint
 
 # ...
 
-users_db_bp = Blueprint('usuarios_db', __name__)
-asignaturas_db_bp = Blueprint('asignaturas_db', __name__)
-pruebas_db_bp = Blueprint('pruebas_db', __name__)
-alumnos_db_bp = Blueprint('alumnos_db', __name__)
-cursos_db_bp = Blueprint('cursos_db', __name__)
-scanner_db_bp = Blueprint('scanner_db', __name__)
-formato_db_bp = Blueprint('formato_db', __name__)
-auth_bp = Blueprint('auth', __name__)
+users_db_bp = Blueprint('usuarios_db', __name__, url_prefix='/users')
+asignaturas_db_bp = Blueprint('alumnos_db', __name__, url_prefix='/alumnos')
+pruebas_db_bp = Blueprint('asignaturas_db', __name__, url_prefix='/asignaturas')
+alumnos_db_bp = Blueprint('cursos_db', __name__, url_prefix='/cursos')
+cursos_db_bp = Blueprint('formato_db', __name__, url_prefix='/formato')
+scanner_db_bp = Blueprint('scanner_db', __name__, url_prefix='/scanner')
+formato_db_bp = Blueprint('pruebas_db', __name__, url_prefix='/pruebas')
+auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
