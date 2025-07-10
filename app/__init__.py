b8a573dff3fe69497409ed3207b4007969a8d006
Joaquín Ignacio <116734135+JoacoIgnacio@users.noreply.github.com>
2025-07-10 17:42:50 -0400

    Update inits

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
-from flask_cors import CORS  # Importa la extensión CORS
-from app.routes.routes_users import users_db_bp 
+from flask_cors import CORS
+
+# Importa los blueprints definidos en sus propios archivos de rutas
+from app.routes.routes_users import users_db_bp
 from app.routes.routes_asignaturas import asignaturas_db_bp
 from app.routes.routes_pruebas import pruebas_db_bp
 from app.routes.routes_alumnos import alumnos_db_bp
@@ -8,12 +10,10 @@ from app.routes.routes_cursos import cursos_db_bp
 from app.routes.routes_scanner import scanner_db_bp
 from app.routes.routes_formato import formato_db_bp
 from app.routes.auth_routes import auth_bp
+
 app = Flask(__name__)
 
-# Configuración de la aplicación, si es necesario
-# app.config['DEBUG'] = True
-
-# Registra la blueprint de la API
+# Registra todos los blueprints bajo el prefijo común /back_orm
 app.register_blueprint(users_db_bp, url_prefix='/back_orm')
 app.register_blueprint(asignaturas_db_bp, url_prefix='/back_orm')
 app.register_blueprint(pruebas_db_bp, url_prefix='/back_orm')
@@ -23,7 +23,5 @@ app.register_blueprint(scanner_db_bp, url_prefix='/back_orm')
 app.register_blueprint(formato_db_bp, url_prefix='/back_orm')
 app.register_blueprint(auth_bp, url_prefix='/back_orm')
 
-
-# Configuración de CORS
-CORS(app)  # Esto permite solicitudes desde cualquier origen
-
+# Habilita CORS para frontend móvil
+CORS(app)
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,12 +1,8 @@
-from flask import Blueprint
-
-# ...
-
-users_db_bp = Blueprint('usuarios_db', __name__, url_prefix='/users')
-asignaturas_db_bp = Blueprint('alumnos_db', __name__, url_prefix='/alumnos')
-pruebas_db_bp = Blueprint('asignaturas_db', __name__, url_prefix='/asignaturas')
-alumnos_db_bp = Blueprint('cursos_db', __name__, url_prefix='/cursos')
-cursos_db_bp = Blueprint('formato_db', __name__, url_prefix='/formato')
-scanner_db_bp = Blueprint('scanner_db', __name__, url_prefix='/scanner')
-formato_db_bp = Blueprint('pruebas_db', __name__, url_prefix='/pruebas')
-auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
+from .routes_users import users_db_bp
+from .routes_asignaturas import asignaturas_db_bp
+from .routes_pruebas import pruebas_db_bp
+from .routes_alumnos import alumnos_db_bp
+from .routes_cursos import cursos_db_bp
+from .routes_scanner import scanner_db_bp
+from .routes_formato import formato_db_bp
+from .auth_routes import auth_bp
